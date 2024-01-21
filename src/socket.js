f15e357de9c880e96ec8ec72240556b86b642878
Dhruvil Kakadiya <kakadiyadhruvil3006@gmail.com>
2024-01-21 15:14:08 +0530

    socket updated

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 const socket = 
-io('http://localhost:5000/', {
+io('https://lockouts-server-production.up.railway.app/', {
     transports: ["websocket"],
     upgrade: false
 });
